c260e250fade533b1d37d2f47bff7fbcd9c04276
kiran2303 <kiranbavariya2303@gmailcom>
2017-02-02 10:06:29 +0530

    commit first

--- app/src/main/java/com/example/kiran/loginactivity/LoginActivity.java
+++ app/src/main/java/com/example/kiran/loginactivity/LoginActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+//First commit;
 
 public class LoginActivity extends AppCompatActivity {
 
